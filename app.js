3d7fbbb7caf36682703460bf6e54a935fc4fe5e6
Dhyey Moliya <dhyeymoliya@gmail.com>
2016-12-18 15:49:35 +0530

    Added execute get request route

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
 app.post('/create', routes.create);
+app.get('/execute', routes.execute);
 app.get('/success', routes.success);
 app.get('/cancel', routes.cancel);
 
